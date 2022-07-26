64da3c5c142b656d8d875d986aba53d0ca737074
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2022-07-26 17:54:29 +0300

    Adds mui, eslint plugins, pre-commit script

--- next.config.js
+++ next.config.js
@@ -1,5 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    config.plugins.push(
+      new ESLintPlugin({
+        // Plugin options
+        extensions: ['js', 'mjs', 'jsx', 'ts', 'tsx'],
+        eslintPath: require.resolve('eslint'),
+        context: resolveApp('src'),
+        cache: true,
+        cacheLocation: path.resolve(
+          resolveApp('node_modules'),
+          '.cache/.eslintcache'
+        ),
+        // ESLint class options
+        cwd: resolveApp('.'),
+        resolvePluginsRelativeTo: __dirname,
+        baseConfig: {
+          extends: [require.resolve('eslint-config-react-app/base')],
+          rules: {},
+        },
+      })
+    );
+
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
